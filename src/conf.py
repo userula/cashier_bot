f8c2b13e2cfeef24e10ea242b40cb3f2319ce715
Ulan <u.buzhbanov@kazdream.kz>
2023-10-08 16:09:07 +0600

    [FIX] token

--- src/conf.py
+++ src/conf.py
@@ -5,8 +5,8 @@ env = Env()
 app_name = 'NU cashier bot'
 
 SKIP_UPDATES = False
-# TELEGRAM_TOKEN = env('BOT_TOKEN')
-TELEGRAM_TOKEN = "1239266158:AAF9ES_M4vvKF2zv7pIZCZAtruMfjAPHHj4"
+
+TELEGRAM_TOKEN = env('BOT_TOKEN')
 
 ADMIN_IDS = env.list('ADMIN_IDS')
 
